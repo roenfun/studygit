a3d85d20d2878416f0502ccd4e455ca8a585f2ec
Chad.Long <roenfun@163.com>
2014-08-20 23:42:30 +0800

    step 1: add hello-world maven project to github

---
+++ hello-world/src/main/java/com/test/sample/hello_world/MainClass.java
@@ -0,0 +1,10 @@
+package com.test.sample.hello_world;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
